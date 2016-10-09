386c731b0e53485961106401d7f3a0f5278d6910
nikita-mospan <mospan.nn@phystech.edu>
2016-10-09 23:56:17 +0300

    Updated examples of using PL/SQL Hierarchical Log

--- usage/test_pk_util_log.sql
+++ usage/test_pk_util_log.sql
@@ -1,10 +1,37 @@
+--main hierarchical query
+SELECT    
+    l.start_log_id,
+    LPAD (' ', 2* (LEVEL- 1)) || l.log_id as log_id,
+    l.parent_log_id,
+    l.start_ts,
+    l.end_ts,
+    LPAD (' ', 2* (LEVEL- 1)) || SUBSTR ( (l.end_ts - l.start_ts), 13, 9) AS elapsed,
+    l.sid,
+    l.username,
+    l.status,
+    l.row_count,
+    LPAD (' ', 2* (LEVEL- 1)) || l.comments AS comments,
+    l.exception_message
+FROM
+    tech_log_table l
+START WITH l.log_id = &start_log_id
+CONNECT BY
+    l.parent_log_id = PRIOR l.log_id
+ORDER SIBLINGS BY
+	l.log_id ASC;
+
+--Table that stores hierarchical log instances
+select * from tech_log_instances ; 
+
+--basic logging example
+--note: the shcema must have execute grant on pk_util_log package
 DECLARE
     PROCEDURE b(p_name_in IN VARCHAR2) IS
         v_dummy_cnt PLS_INTEGER;
     BEGIN
         pk_util_log.open_next_level(p_comments_in => 'procedure B(), line: ' || $$PLSQL_LINE || chr(13) || chr(10) ||
                                                      'p_name_in: ' || p_name_in);
-        dbms_lock.sleep(10);
+        dbms_lock.sleep(3);
         pk_util_log.close_level(p_status_in => 'C');
     EXCEPTION
         WHEN OTHERS THEN
@@ -17,7 +44,7 @@ DECLARE
         pk_util_log.open_next_level('procedure A(), line: ' || $$PLSQL_LINE || chr(13) || chr(10) ||
                                     'p_name_in: ' || p_name_in);
         b('dummy_b');
-        dbms_lock.sleep(5);
+        dbms_lock.sleep(2);
         pk_util_log.close_level(p_status_in => pk_util_log.g_status_completed);
     EXCEPTION
         WHEN OTHERS THEN
@@ -32,42 +59,58 @@ BEGIN
 END;
 /
 
-SELECT    
-	l.start_log_id,
-    LPAD (' ', 2* (LEVEL- 1)) || l.log_id,
-    l.parent_log_id,
-    l.start_ts,
-    l.end_ts,
-    LPAD (' ', 2* (LEVEL- 1)) || SUBSTR ( (l.end_ts - l.start_ts), 13, 9) AS elapsed,
-    l.sid,
-    l.username,
-    l.status,
-    l.row_count,
-    LPAD (' ', 2* (LEVEL- 1)) || l.comments AS comments,
-    l.exception_message
-FROM
-	tech_log_table l
-START WITH l.log_id = 103
-CONNECT BY
-	l.parent_log_id = PRIOR l.log_id
-ORDER SIBLINGS BY
-	l.log_id ASC;
+--sql row_count example
+declare
+    v_sql varchar2(32767) := 'select 1 from dual';
+    v_dummy_value pls_integer;
+BEGIN
+    pk_util_log.stop_logging;
+    pk_util_log.set_log_name(p_name_in => 'sql_rowcount_example');
+    pk_util_log.open_next_level(p_comments_in => v_sql);
+    execute immediate v_sql into v_dummy_value;
+    pk_util_log.close_level(p_status_in => pk_util_log.g_status_completed, p_row_count_in => sql%rowcount);
+EXCEPTION
+    WHEN OTHERS THEN
+        pk_util_log.close_level(p_status_in => pk_util_log.g_status_failed);
+        RAISE;
+END;
+/
 
+--resume logging example
+--note: the shcema must have grants to create dbms_scheduler jobs. For details please refer to Oracle documentation
 declare
     v_plsql_block varchar2(32767);
     v_last_log_id tech_log_table.log_id%type;
+    v_cur_log_id tech_log_table.log_id%type;
 BEGIN
-    v_last_log_id  := pk_util_log.get_current_log_id;
-    dbms_output.put_line(v_last_log_id); 
-    v_plsql_block :=
-        'begin
-            pk_util_log.resume_logging(p_parent_log_id => #log_id#);
-            pk_util_log.log_record(p_comments_in => ''Job record'', p_status_in => pk_util_log.g_status_completed);
-        end;';
-    dbms_scheduler.create_job(job_name        => 'resume_logging_job'
-                             ,job_type        => 'PLSQL_BLOCK'
-                             ,job_action      => replace(v_plsql_block, '#log_id#', v_last_log_id)
-                             ,enabled         => TRUE
-                             , auto_drop => TRUE);
+    pk_util_log.stop_logging;
+    pk_util_log.set_log_name('resume_logging_example');
+    pk_util_log.open_next_level(p_comments_in => 'Some comment');
+    v_cur_log_id := pk_util_log.get_current_log_id;
+    for i  in 1 .. 5 loop
+        v_plsql_block :=
+            'begin
+                pk_util_log.resume_logging(p_parent_log_id => #log_id#);
+                pk_util_log.open_next_level(p_comments_in => ''Job record'');
+                dbms_lock.sleep(#i#);
+                pk_util_log.close_level(p_status_in => pk_util_log.g_status_completed);
+            exception
+	            when others then
+                    pk_util_log.close_level(p_status_in => pk_util_log.g_status_failed);
+                    raise;
+            end;';
+        dbms_scheduler.create_job(job_name        => 'RESUME_LOGGING_JOB_' || to_char(i)
+                                 ,job_type        => 'PLSQL_BLOCK'
+                                 ,job_action      => replace(replace(v_plsql_block, '#log_id#', to_char(v_cur_log_id)), 
+                                                            '#i#', to_char(i))
+                                 ,enabled         => TRUE
+                                 , auto_drop => TRUE);
+    end loop;
+    pk_util_log.close_level(p_status_in => pk_util_log.g_status_completed);
+exception
+	when others then
+		pk_util_log.close_level(p_status_in => pk_util_log.g_status_failed);
+		raise;
 END;
 /
+ 
