c109e0103bc8596350f229c448af75646109f35b
Nikita Mospan <mospan.nn@phystech.edu>
2018-09-28 00:57:44 +0300

    Add DATE_TIMESTAMP_BK_TST_M entity

--- ETL/scripts/entity_metadata_model.sql
+++ ETL/scripts/entity_metadata_model.sql
@@ -9,14 +9,14 @@ drop table master_tech_attributes;
 drop table sources;
 
 create table master_tables (
-    master_table varchar2(30),
+    master_table varchar2(23),
     auxillary_table as (substr(master_table, 1, length(master_table) - 1) || 'A'),
     staging_table as (substr(master_table, 1, length(master_table) - 1) || 'S'),
     constraint c_master_table_pk primary key(master_table)
 );
 
 create table master_tables_attributes (
-    master_table varchar2(30) not null,
+    master_table varchar2(23) not null,
     attribute_name varchar2(30) not null,
     attribute_type varchar2(100) not null,
     is_part_of_business_key varchar2(1) check (is_part_of_business_key in ('Y', 'N')),
@@ -33,7 +33,7 @@ create table master_tech_attributes (
     constraint c_master_tech_attributes_pk primary key (attribute_name)); 
 
 create table mappings2etl_stage (
-    mapping_name varchar2(30),   
+    mapping_name varchar2(100),   
     mapping_sql  clob ,
     constraint c_mappings2master_pk primary key (mapping_name) );
     
@@ -52,6 +52,8 @@ union all
 select 'VALID_RULES_CONFIG_M' from dual
 union all
 select 'COMPANY_ERR_M' from dual
+union all
+select 'DATE_TIMESTAMP_BK_TST_M' from dual
 ;
 
 insert into master_tables_attributes (
@@ -66,7 +68,9 @@ select 'VALID_RULES_CONFIG_M',     'RULE_DESCRIPTION', 'VARCHAR2(4000) NOT NULL'
 select 'VALID_RULES_CONFIG_M',     'VALIDATION_CHECK', 'VARCHAR2(4000) NOT NULL', 'N',            'Y',                  NULL          from dual union all
 select 'COMPANY_ERR_M', 'RULE_CODE', 'VARCHAR2(30) NOT NULL', 'Y', 'N', 1 from dual union all
 select 'COMPANY_ERR_M', 'SOURCE_X_BUSINESS_HKEY', 'VARCHAR2(32) NOT NULL', 'Y', 'N', 2 from dual union all
-select 'COMPANY_ERR_M', 'SOURCE_X_VSTART', 'TIMESTAMP(6) NOT NULL', 'Y', 'N', 3 from dual 
+select 'COMPANY_ERR_M', 'SOURCE_X_VSTART', 'TIMESTAMP(6) NOT NULL', 'Y', 'N', 3 from dual union all
+select 'DATE_TIMESTAMP_BK_TST_M',     'DATE_ID',       'DATE NOT NULL',           'Y',                     'N',                 1          from dual union all
+select 'DATE_TIMESTAMP_BK_TST_M',     'TIMESTAMP_ID',       'TIMESTAMP(6) NOT NULL',           'Y',                     'N',                 2          from dual
 ;
 
 insert into master_tech_attributes (
@@ -89,7 +93,9 @@ select  'POPULATE_VALID_RULES_CONFIG',
                                    select 'RULE_1' as rule_code,
                                             'COMPANY_M' as validated_table,
                                             'SEGMENT field must be uppercase' as rule_description,
-                                            'segment = upper(segment) ' as validation_check from dual}' from dual
+                                            'segment = upper(segment) ' as validation_check from dual}' from dual union all
+select 'POPULATE_DATE_TIMESTAMP_BK_TST_M', q'{insert into DATE_TIMESTAMP_BK_TST_S (DATE_ID, TIMESTAMP_ID) values (sysdate, systimestamp)}' from dual
+    
 ; 
 
 commit;
